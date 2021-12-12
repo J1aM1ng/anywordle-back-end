f9d86ccd9096b6cbd1ce19b861ed2e612ad2de70
J1aM1ng <2605379072@qq.com>
2021-12-12 21:31:22 +0800

    init: back-end

---
+++ src/controllers/auth.ts
@@ -0,0 +1,12 @@
+// src/controllers/auth.ts
+import { Context } from "koa";
+
+export default class AuthController {
+  public static async login(ctx: Context) {
+    ctx.body = "Login controller";
+  }
+
+  public static async register(ctx: Context) {
+    ctx.body = "Register controller";
+  }
+}
---
+++ src/entity/user.ts
@@ -0,0 +1,18 @@
+// 用户模型
+// src/entity/user.ts
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ select: false })
+  password: string;
+
+  @Column()
+  email: string;
+}
